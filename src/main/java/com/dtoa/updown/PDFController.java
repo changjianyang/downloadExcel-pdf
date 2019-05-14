3fddb8544acb7e3ef14293249400cd3c25df7241
yangchangjian <cjyang1995@126.com>
2019-05-14 14:36:39 +0800

    导入导出excel&pf的demo 修改pdf合并行列问题

--- src/main/java/com/dtoa/updown/PDFController.java
+++ src/main/java/com/dtoa/updown/PDFController.java
@@ -36,150 +36,116 @@ public class PDFController {
             document.open();
 
             //第0行
-            PdfPTable table0 = new PdfPTable(4);
+            PdfPTable table = new PdfPTable(4);
             PdfPCell cell0 = new PdfPCell();
             cell0.setColspan(4);
             cell0.setPhrase(new Paragraph("aa的日志", font));
             cell0.setHorizontalAlignment(Element.ALIGN_CENTER); //水平居中
             cell0.setPaddingBottom(10);
             cell0.setBorderColor(BaseColor.WHITE);
-            table0.addCell(cell0);
-            document.add(table0);
+            table.addCell(cell0);
 
             //第一行
-            PdfPTable table = new PdfPTable(4);
             PdfPCell cell = new PdfPCell();
             cell.setHorizontalAlignment(Element.ALIGN_CENTER); //水平居中
             cell.setBackgroundColor(BaseColor.LIGHT_GRAY);
             cell.setPhrase(new Paragraph("汇报时间", font));
             table.addCell(cell);
-            document.add(table);
+
 
             cell = new PdfPCell();
             cell.setPhrase(new Paragraph("2019/01/01", font));
             table.addCell(cell);
-            document.add(table);
 
             cell = new PdfPCell();
             cell.setPhrase(new Paragraph("模板名称", font));
             cell.setBackgroundColor(BaseColor.LIGHT_GRAY);
             cell.setHorizontalAlignment(Element.ALIGN_CENTER); //水平居中
             table.addCell(cell);
-            document.add(table);
 
             cell = new PdfPCell();
             cell.setPhrase(new Paragraph("日报", font));
             table.addCell(cell);
-            document.add(table);
 
             //第2行
-            PdfPTable table2 = new PdfPTable(4);
             PdfPCell cell2 = new PdfPCell();
             cell2.setHorizontalAlignment(Element.ALIGN_CENTER); //水平居中
             cell2.setBackgroundColor(BaseColor.LIGHT_GRAY);
             cell2.setPhrase(new Paragraph("汇报人", font));
 
-            table2.addCell(cell2);
-            document.add(table2);
+            table.addCell(cell2);
 
             cell2 = new PdfPCell();
             cell2.setPhrase(new Paragraph("aa", font));
-            table2.addCell(cell2);
-            document.add(table2);
+            table.addCell(cell2);
 
             cell2 = new PdfPCell();
             cell2.setPhrase(new Paragraph("汇报人部门", font));
             cell2.setBackgroundColor(BaseColor.LIGHT_GRAY);
             cell2.setHorizontalAlignment(Element.ALIGN_CENTER); //水平居中
 
-            table2.addCell(cell2);
-            document.add(table2);
+            table.addCell(cell2);
 
             cell2 = new PdfPCell();
             cell2.setPhrase(new Paragraph("公司", font));
-            table2.addCell(cell2);
-            document.add(table2);
+            table.addCell(cell2);
 
             //第3行
-            PdfPTable table3 = new PdfPTable(4);
             PdfPCell cell3 = new PdfPCell();
             cell3.setBackgroundColor(BaseColor.LIGHT_GRAY);
             cell3.setPhrase(new Paragraph("汇报对象", font));
             cell3.setHorizontalAlignment(Element.ALIGN_CENTER); //水平居中
 
-            table3.addCell(cell3);
-            document.add(table3);
+            table.addCell(cell3);
 
             cell3 = new PdfPCell();
             cell3.setColspan(3);
             cell3.setPhrase(new Paragraph("刘兵", font));
-            table3.addCell(cell3);
-            document.add(table3);
+            table.addCell(cell3);
 
             //第4行
-            PdfPTable table4 = new PdfPTable(4);
             PdfPCell cell4 = new PdfPCell();
             cell4.setColspan(4);
             cell4.setPhrase(new Paragraph("\n", font));
-            table4.addCell(cell4);
-            document.add(table4);
+            table.addCell(cell4);
 
             //第5行
-            PdfPTable table5 = new PdfPTable(4);
             PdfPCell cell5 = new PdfPCell();
             cell5.setColspan(4);
             cell5.setBackgroundColor(BaseColor.LIGHT_GRAY);
             cell5.setPhrase(new Paragraph("汇报对象", font));
             cell5.setHorizontalAlignment(Element.ALIGN_CENTER); //水平居中
 
-            table5.addCell(cell5);
-            document.add(table5);
+            table.addCell(cell5);
 
             //第6行
-            PdfPTable table6 = new PdfPTable(4);
             PdfPCell cell6 = new PdfPCell();
             cell6.setPhrase(new Paragraph("今日计划", font));
             cell6.setBackgroundColor(BaseColor.LIGHT_GRAY);
             cell6.setHorizontalAlignment(Element.ALIGN_CENTER); //水平居中
+            cell6.setRowspan(3);
+            table.addCell(cell6);
 
-            table6.addCell(cell6);
-            document.add(table6);
 
             cell6 = new PdfPCell();
             cell6.setColspan(3);
             cell6.setPhrase(new Paragraph("好好学习", font));
-            table6.addCell(cell6);
-            document.add(table6);
+            table.addCell(cell6);
+
 
             //第7行
-            PdfPTable table7 = new PdfPTable(4);
+            // PdfPTable table7 = new PdfPTable(3);
             PdfPCell cell7 = new PdfPCell();
-            cell7.setPhrase(new Paragraph("明日计划", font));
-            cell7.setBackgroundColor(BaseColor.LIGHT_GRAY);
-            cell7.setHorizontalAlignment(Element.ALIGN_CENTER); //水平居中
-            table7.addCell(cell7);
-            document.add(table7);
-
-            cell7 = new PdfPCell();
             cell7.setColspan(3);
             cell7.setPhrase(new Paragraph("好好学习2", font));
-            table7.addCell(cell7);
-            document.add(table7);
+            table.addCell(cell7);
 
             //第8行
-            PdfPTable table8 = new PdfPTable(4);
             PdfPCell cell8 = new PdfPCell();
-            cell8.setPhrase(new Paragraph("其他事项", font));
-            cell8.setBackgroundColor(BaseColor.LIGHT_GRAY);
-            cell8.setHorizontalAlignment(Element.ALIGN_CENTER); //水平居中
-            table8.addCell(cell8);
-            document.add(table8);
-
-            cell8 = new PdfPCell();
             cell8.setColspan(3);
             cell8.setPhrase(new Paragraph("暂无", font));
-            table8.addCell(cell8);
-            document.add(table8);
+            table.addCell(cell8);
+            document.add(table);
 
             document.close();
         } catch (Exception e) {
